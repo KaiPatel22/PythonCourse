8192ae73c390d4f09ebe685d06fcd049a3996230
Kai Patel <kaipatel424@gmail.com>
2025-06-13 10:42:36 +0100

    List exercises completed

---
+++ listExercise.py
@@ -0,0 +1,15 @@
+'''
+Given a list of integer values, write a Python program to check whether 
+the list contains the same number in adjacent positions. 
+Display the count of such adjacent occurrences.
+'''
+
+def countAdjacentOccurences(numList):
+    count = 0
+    for i in range(1, len(numList)):
+        print(f"Comparing {numList[i]} and {numList[i - 1]}")
+        if numList[i] == numList[i - 1]:
+            count += 1
+    return count
+
+print(countAdjacentOccurences([1,1,5,100,-20,-20,6,0,0]))
